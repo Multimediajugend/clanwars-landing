206868c49971b5cb8853fa8e64089a50d6fb80bc
Martin Meyer <mmeyer@transim.com>
2016-08-18 18:52:18 +0200

    Add logger

--- config/config.example.php
+++ config/config.example.php
@@ -6,4 +6,8 @@ define("FB_APPID", "--YourAppID--");
 define("DATABASE_HOST", "localhost"); 
 define("DATABASE_USER", "username"); 
 define("DATABASE_PASS", "password"); 
-define("DATABASE_NAME", "my_database"); 
+define("DATABASE_NAME", "my_database");
+// Your mail-settings:
+define("MAIL_HOST", "smtp.office365.com");
+define("MAIL_USER", "domain.com\user@domain.com"); // In case of using Office365 it should look like: 
+define("MAIL_PASS", "password"); 
---
+++ loggertest.php
@@ -0,0 +1,19 @@
+<?php
+// error_reporting(E_ALL);
+require 'vendor/autoload.php';
+
+$users = [
+    [
+        'name' => 'Kenny Katzgrau',
+        'username' => 'katzgrau',
+    ],
+    [
+        'name' => 'Dan Horrigan',
+        'username' => 'dhrrgn',
+    ],
+];
+
+$logger = new Katzgrau\KLogger\Logger(__DIR__.'/logs');
+$logger->info('Returned a million search results');
+$logger->error('Oh dear.');
+$logger->debug('Got these users from the Database.', $users);
