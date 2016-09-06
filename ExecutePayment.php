0e527ddb991281b1dec8994f3db057dc3526cf00
Martin Meyer <mmeyer@transim.com>
2016-09-06 16:52:39 +0200

    Add implementation for cancelUrl

--- ExecutePayment.php
+++ ExecutePayment.php
@@ -1,32 +1,43 @@
 <?php
-require_once('./config/config.php');
-require_once('./backend/paypal.php');
-require_once('./backend/paymentdb.php');
-require_once('./backend/clan.php');
-require_once('./vendor/autoload.php');
+require_once dirname(__FILE__) . "/backend/paypaldb.php";
 
-if(isset($_GET['success']) && $_GET['success'] == 'true') {
-    if(isset($_GET['paymentId']) && isset($_GET['PayerID']))
-    {
-        $paymentId = $_GET['paymentId'];
-        $execution->setPayerId($_GET['PayerID']);
+$paypal = new PayPalDB();
 
-        $paypal = new PayPal();
-
-        if($paypal->paymentSuccess($paymentId, $payerId)) {
-            // TODO: sent mail to guest and info
-        } else {
-            // TODO: sent mail to guest and info
-        }
-
-        // TODO: redirect to main-page
-        exit(0);
+if(isset($_GET['success'])) {
+    if(isset($_GET['token'])) {
+        $token = $_GET['token'];
     } else {
-        echo "Missing Parameters..";
-        exit(1);
+        redirect();
     }
-} else {
-    // TODO: redirect to main-page
-    echo "User Cancelled the Approval";
-    exit;
+
+    if($_GET['success'] == 'true') {
+        if(isset($_GET['paymentId']) && isset($_GET['PayerID']))
+        {
+            $paymentId = $_GET['paymentId'];
+            $execution->setPayerId($_GET['PayerID']);
+
+            $paypal = new PayPalDB();
+
+            if($paypal->paymentSuccess($paymentId, $payerId)) {
+                // TODO: sent mail to guest and info
+            } else {
+                // TODO: sent mail to guest and info
+            }
+
+            // TODO: redirect to main-page
+            exit(0);
+        } else {
+            echo "Missing Parameters..";
+            exit(1);
+        }
+    } else {
+        $paypal->cancelPayment($token);
+        redirect('#Anmeldung');
+    }
+}
+redirect();
+
+function redirect($param) {
+    header('Location: /'.$param);
+    exit();
 }
